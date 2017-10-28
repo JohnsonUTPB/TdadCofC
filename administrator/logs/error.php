c61b5d83213e40ddeb2a8019f308062ca8de4a84
Joshua Johnson <JJohnsonDeveloped@gmail.com>
2017-10-27 18:08:12 -0600

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-10-27 03:54:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-10-27T03:54:21+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-10-27T03:54:43+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-10-27T22:46:58+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-10-27T22:47:15+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
